04819626455d0f8eac4ccc9a826bed7ef9d4e7fa
Ulric19 <karlusimon@gmail.com>
2015-05-13 13:35:58 -0300

    -Added striping to /view/InstancesView.js

--- src/view/InstancesView.js
+++ src/view/InstancesView.js
@@ -5,45 +5,46 @@
 var InstancesView = Backbone.View.extend({
 
     className: 'InstancesView',
-    
-	initialize: function(options) {
-        
-        if(!this.model) {
+
+    initialize: function(options) {
+
+        if (!this.model) {
             this.model = new InstancesModel();
         }
-  
+
         this.model.addEC2Instance();
 
         this.bindings();
         this.render();
-	},
+    },
     //Check for when the data is read and renders the page
     bindings: function() {
         this.model.change('dataReady', function(model, val) {
-        this.render();
+            this.render();
 
-        $(function() {
-            // call the tablesorter plugin
-            $("table").tablesorter({
-            theme : 'default',
-            // header layout template; {icon} needed for some themes
-            headerTemplate : '{content}{icon}',
-            // initialize zebra striping and column styling of the table
-      });
+            $(function() {
+                // call the tablesorter plugin
+                $.tablesorter.defaults.widgets = ['zebra'];
+                $("table").tablesorter({
+                    theme: 'blue',
+                    
+                    // header layout template; {icon} needed for some themes
+                    headerTemplate: '{content}{icon}',
+                    // initialize zebra striping and column styling of the table
+                });
 
-});
-    }.bind(this));
+            });
+        }.bind(this));
 
     },
 
-	render: function() {		
+    render: function() {
         var html = Handlebars.templates.InstancesView({
             instances: instanceCollection.toJSON()
 
         });
         this.$el.html(html);
-	}
-    
+    }
 
 
 
