6402d107896b65b69674d94389b3c41b7f8d46b3
ar1o <ario.khosh@gmail.com>
2015-06-03 15:58:02 -0300

    Delete dbcheck.js

--- dbcheck.js
+++
@@ -1,5 +0,0 @@
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
-var db = 'mongodb://localhost:27017/billingtestdb';
-
-db.system.namespace.find({name:'billingtestdb.latest'});
