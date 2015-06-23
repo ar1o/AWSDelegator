e95d2e9e52cda128a85149ae131215fa89364a29
dpkm95 <dpkm95@yahoo.com>
2015-06-23 18:47:16 -0300

    operations aws&instance level

---
+++ src/view/AWSOperationsView.js
@@ -0,0 +1,63 @@
+var AWSOperationsView = Backbone.View.extend({
+    className: 'AWSOperationsView',
+
+    initialize: function(options) {
+        if (!this.model) {
+            this.model = new OperationsModel();
+        }
+        this.bindings();
+        this.render();
+    },
+
+    bindings: function() {
+        var self=this;
+        this.model.change('dataReady', function(model, val) {
+            self.render();
+            console.log(operationsCollection);
+            var dataOperations = [];
+            for (var i = 0; i < operationsCollection.length; i++) {
+                dataOperations.push([operationsCollection.at(i).get('operation'), operationsCollection.at(i).get('percentage')]);
+            }
+            $(function() {
+                $('#operationscontainer').highcharts({
+                    chart: {
+                        plotBackgroundColor: null,
+                        plotBorderWidth: null,
+                        plotShadow: false,
+                    },
+                    title: {
+                        text: 'Operations Percentage'
+                    },
+                    tooltip: {
+                        pointFormat: '{series.name}: <b>USD{point.y:.4f}</b>'
+                    },
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: true,
+                                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                                style: {
+                                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                                }
+                            }
+                        }
+                    },
+                    series: [{
+                        type: 'pie',
+                        name: 'Operations',
+                        data: dataOperations
+                    }]
+                });
+            });
+        });
+    },
+
+    render: function() {
+        var html = Handlebars.templates.OperationsView({
+            metrics: operationsCollection.toJSON()
+        });
+        this.$el.html(html);
+    }
+});
