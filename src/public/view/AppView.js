3fb301a9d276e14a9fdacb5fe26bd8224b15bff4
ar1o <ario.khosh@gmail.com>
2015-08-27 14:43:39 -0300

    AppView commented and cleaned

--- src/public/view/AppView.js
+++ src/public/view/AppView.js
@@ -1,12 +1,11 @@
 var AppView = Backbone.View.extend({
-
+    /* run render, bind and initialize other views */
     initialize: function(options) {
         AppView.sharedInstance = this;
         var self = this;
 
         this.header = new HeaderView();
         this.navView = new NavView();
-        // this.meterActivity = new MeterView();
         this.latestTime = new TimeView();
         this.budgetView = new BudgetView();
         this.timeBudgetView = new TimeBudgetView();
@@ -15,62 +14,42 @@ var AppView = Backbone.View.extend({
 
         this.timeBudgetView.model.getTimeBudgets();
         this.footer = new FooterView();
+
         this.router = new AppRouter({
             defaultView: 'AWSView'
         });
 
-
         this.bindings();
         this.render();
         this.setListeners();
-        $('.content-view').append(this.latestTime.el);
 
+        $('.content-view').append(this.latestTime.el);
         $('.content-view').append(this.footer.el);
 
+        // content view that holds all the content heigh calculation
         window_size = $(window).height();
-        // console.log(window_size);
         var length_calc = (window_size - 50);
         var length = length_calc + 'px';
         this.$('.content-view').css({
             'height': length
         });
 
-
         $(window).resize(function() {
             window_size = $(window).height();
-            // console.log(window_size);
             var length_calc = (window_size - 50);
             var length = length_calc + 'px';
             this.$('.content-view').css({
                 'height': length
             });
-
-
         });
-
-
-// var docHeight = $(".content-view").height();
-// var footerHeight = $('.FooterView').height();
-// console.log(docHeight);
-// console.log(footerHeight);
-// var footerTop = $('.FooterView').position().top + footerHeight;
-
- // if (footerTop < docHeight) {
- //    $('.FooterView').css('margin-top', 10+ (docHeight - footerTop) + 'px');
- //   }
-        // var footerResize = function() {
-        //     $('.FooterView').css('position', $(".content-view").height() + $(".FooterView").innerHeight() > $(window).height() ? "inherit" : "fixed");
-        // };
-        // $(window).resize(footerResize).ready(footerResize);
-        // console.log(footerResize());
     },
 
     setListeners: function() {
         var self = this;
+        // url changes drive location within the app
         this.router.on("change:view", function(a, view) {
             self.setView(view);
         });
-
         var prevArgs = {};
         this.router.on('change:args', function(a, args) {
             var currentView = self.model.get('currentView');
@@ -87,18 +66,20 @@ var AppView = Backbone.View.extend({
         });
     },
 
+    /* make our view listen to user events */
     bindings: function() {
+        // for displaying tooltips
         $(document).ready(function() {
             $('[data-toggle="tooltip"]').tooltip();
         });
 
+        // user clicks on the menu icon should open the nav view
         this.$el.on("click", '.menu', function(e) {
             if (this.navView.model.isOpen == true) {
                 this.navView.model.isOpen = false;
             } else {
                 this.navView.model.isOpen = true;
                 window_size = $(window).height();
-                // console.log(window_size);
                 var length_calc = (window_size);
                 var length = length_calc + 'px';
                 this.$('.NavView').css({
@@ -107,6 +88,7 @@ var AppView = Backbone.View.extend({
             }
         }.bind(this));
 
+        // users mouse hovers the menu icon should open the nav view
         this.$el.on("mouseenter", '.menu', function(e) {
             this.navView.model.isOpen = true;
             window_size = $(window).height();
@@ -117,20 +99,25 @@ var AppView = Backbone.View.extend({
             });
         }.bind(this));
 
+        // users mouse is on the nav view it should be kept open
         this.$el.on("mouseenter", '.NavView', function(e) {
             this.navView.model.isOpen = true;
         }.bind(this));
 
+        //users mouse leaves the nav view it should close
         this.$el.on("mouseleave", '.NavView', function(e) {
             this.navView.model.isOpen = false;
         }.bind(this));
 
+        // users mouse leaves the menu icon it should close
         this.$el.on("mouseleave", '.menu', function(e) {
             this.navView.model.isOpen = false;
         }.bind(this));
 
+        // this does nothing
         this.$el.on("click", '.setting', function(e) {}.bind(this));
 
+        // handle nav view clicks
         this.$el.on('click', '[page-id="0"]', function(e) {
             this.navView.model.isOpen = false;
             window.location.hash = '#/AWS';
@@ -175,17 +162,18 @@ var AppView = Backbone.View.extend({
             this.navView.model.isOpen = false
             window.location.hash = '#/UMTimeBudgets';
         }.bind(this));
+        // end of handling nav view clicks
 
+        // user clicks notification icon should open the notification view
         this.$el.on("click", '.notify', function(e) {
-            // console.log('notification clicked')
             if (this.notificationView.model.isOpen == false) {
                 this.notificationView.model.isOpen = true;
-                // this.$( ".mdl-badge" ).removeAttr( "data-badge");
             } else {
                 this.notificationView.model.isOpen = false;
             }
         }.bind(this));
 
+        // the notification view badge should be update according to the notificaiton available
         this.notificationView.model.change('dataReady', function(model, val) {
             if (this.notificationView.model.getSeenNumber() == 0) {
                 this.$(".mdl-badge").removeAttr("data-badge");
@@ -196,25 +184,22 @@ var AppView = Backbone.View.extend({
 
     },
 
+    /* render the app view */
     render: function() {
         this.$el.html(Handlebars.templates.AppView({
             time: this.time
-
         }));
         this.$el.append(this.header.el);
         this.$el.append(this.navView.el);
         this.$el.append(this.configurationView.el);
-        // this.$el.append(this.footer.el);
         this.$el.append(this.budgetView.el);
         this.$el.append(this.timeBudgetView.el);
         this.$el.append(this.notificationView.el);
 
-        // this.$el.append(this.meterActivity.el);       
-        // this.$el.append(this.latestTime.el);
-        // this.$('.content-view').append(this.footer.el);
         this.setView(this.router.get('view'));
     },
 
+    /* set the corresponding view and destroy the old view on hash changes */
     setView: function(view) {
         var args = this.router.get('args');
         args.parentView = this;
@@ -226,6 +211,6 @@ var AppView = Backbone.View.extend({
         this.$el.find('.content-view').html(viewInstance.el);
         $('.content-view').append(this.footer.el);
         $('.content-view').append(this.latestTime.el);
-
     }
+
 });
